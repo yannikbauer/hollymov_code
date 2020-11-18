4550665ae11e85ee581c5d30f6b949df1627742a
ralu4196 <lisa.schmors@uni-oldenburg.de>
2020-11-18 17:29:12 +0100

    hmov_utils.py: del duplicate functions (now in djd/hmov.py), update notebook

--- hmov_code/hmov_utils.py
+++ hmov_code/hmov_utils.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 from lxml import etree
 # Import DJD modules / tables
 import datajoint as dj
+from djd import hmov
 from djd import util
 from djd import stimulus
 from djd.event import Event
@@ -63,284 +64,6 @@ def get_xptranges_spont(key):
     return xptranges
 
 
-def get_all_tranges(key, include_spont=False):
-    """Get time ranges of all seconds.
-    For the hollymov stimulus, every second there is a 50/50 chance of optogenetic stimulation.
-    Event.Times() only provides the time ranges of the seconds with opto stimulation. To
-    plot neuron activity w.r.t. opto/no opto condition using unit.psth(), all time ranges
-    are required: the time ranges of seconds with opto stimulation and without stimulation.
-
-    Parameters
-    ----------
-    key : dict
-        Unit key in the form {mouse ID, series, experiment, unit}
-        E.g., {'m': 'Ntsr1Cre_2019_0003', 's': 4, 'e': 9}
-    include_spont: bool
-        Optional include time ranges during gray screen (measuring spontaneous activity 60 s
-        before and after the Hollywood movie is played).
-
-
-    Returns
-    -------
-    tranges_all : np.ndarray
-        Time ranges of all seconds during hmov presentation (not only the ones with opto
-        stimulation) excluding spontaneous opto stimulation before and after the movie.
-
-    """
-    tranges, _, _ = (Unit().Spikes() & key).get_tranges()
-    tranges_opto = (Event.Times() & key & {'ev_chan':'opto1'}).fetch1('ev_tranges')
-    # check that opto stimulations are not longer than 1 s
-    if any(np.diff(tranges_opto) > 1):
-        print("Opto stimulations longer than 1 sec detected. Including only opto stimulations "
-              "of 1 sec.")
-        tranges_opto = np.delete(tranges_opto, np.where(np.diff(tranges_opto) > 1.1)[0], axis=0)
-    trange_last_stop = 0.0
-    tranges_all_l = []
-    for i, trange in enumerate(tranges_opto):
-        trange_current_start = trange[0]
-        diff_opto = trange_current_start - trange_last_stop
-        # if there is opto stim in the next second
-        if diff_opto <= 0.9:
-            tranges_all_l.append(trange)
-            trange_last_stop = trange[1]
-        # if there is approx. 1 sec between opto stimulations
-        elif (diff_opto > 0.9) & (diff_opto <= 1.0):
-            tranges_all_l.append(np.array([trange_last_stop, trange_current_start]))
-            tranges_all_l.append(trange)
-            trange_last_stop = trange[1]
-        # if there is one or more seconds without opto stimulation
-        else:
-            # if there is no opto stim in the next second
-            # check the number of seconds before the next opto stim
-            tranges_num = int(diff_opto)
-            # get the total time lag between each new trange caused by a jitter
-            stim_jitter = diff_opto - tranges_num
-            # get the estimated time jitter for each new trange (should be ~0.002 s)
-            stim_jitter_each = stim_jitter / (tranges_num + 1)
-            # loop through number of elements needed to be inserted
-            for trange_i in range(tranges_num):
-                # create a new start and stop time w.o. opto that takes jitter into account
-                trange_i_start = round(trange_last_stop + stim_jitter_each, 4)
-                trange_i_stop = trange_i_start + 1
-                tranges_all_l.append(np.array([trange_i_start, trange_i_stop]))
-                trange_last_stop = trange_i_stop
-                # update new last trange stop
-            tranges_all_l.append(trange)
-            trange_last_stop = trange[1]
-    tranges_all = np.vstack(tranges_all_l)
-
-    if include_spont == False:
-        # only include tranges during movie presentation
-        tranges_all = np.squeeze(tranges_all[
-            np.where(np.logical_and(tranges_all[:,0] >= tranges[0][0],
-                                    tranges_all[:,1] <= tranges[-1][1])), :])
-    return tranges_all
-
-
-def get_tranges_cond_swit(tranges_in):
-    """Get those time ranges that mark a condition switch.
-    Condition switch appears when in the previous second the opto condition was the opposite.
-    E.g., use all opto time ranges that mark an opto stimulation and get time ranges
-    only of those seconds that have no opto stimulation in the second before. Or give only time
-    ranges without opto stimulation and get time ranges with opto stimulation in the second
-    before.
-
-    Parameters
-    ----------
-    tranges_in : np.ndarray
-        Time ranges of seconds with optogenetic stimulation or no optogenetic stimulation.
-        e.g., (Event.Times() & evkey).fetch('ev_tranges')
-
-    Returns
-    -------
-    tranges_w_diff_cond_bef : np.ndarray
-        Time ranges of those seconds that have a different condition in the second before.
-    """
-    tranges_flatten = tranges_in.flatten()
-    # difference between all tracked event times (also off times)
-    tranges_diff_all = np.diff(tranges_flatten)
-    # delete every second element because that is the stimulation length of 1 s
-    tranges_diff = np.delete(tranges_diff_all, np.arange(0, tranges_diff_all.size, 2))
-    # get the idx where a pause is happening
-    idx_pause = np.where(tranges_diff > 0.9)
-    # add the first element as ON time
-    # (would otherwise be ignored through diff()); add +1 to get the correct index
-    idx_onset = np.insert((np.add(idx_pause, 1)), 0, 0)
-    # get all real opto ON times with no opto stim in the second before
-    onset_time_w_diff_cond_bef = tranges_in[idx_onset, 0]
-
-    evtranges = np.empty((1, 2))
-    # creating an event time ranges vector that has the true ON and OFF times
-    for i in range(idx_onset.shape[0]):
-        evon = onset_time_w_diff_cond_bef[i]
-        evoff = onset_time_w_diff_cond_bef[i] + 1
-        # append the new array with the selected element
-        evtranges_i = np.column_stack((evon, evoff))
-        evtranges = np.row_stack((evtranges, evtranges_i))
-    # account for the empty entry
-    tranges_w_diff_cond_bef = evtranges[1:, :]
-    return tranges_w_diff_cond_bef
-
-
-def get_tranges_cond_cont(tranges):
-    """Get those time ranges where the condition is continued.
-    E.g., input would be all opto time ranges that mark an opto stimulation
-    (Event.Times() & evkey).fetch('ev_tranges') and output would be the time ranges of only
-    those seconds that have also an opto stimulation in the second before (or vice versa).
-
-    Parameters
-    ----------
-    tranges_in : np.ndarray
-        Time ranges of seconds with optogenetic stimulation or no optogenetic stimulation.
-        e.g., (Event.Times() & evkey).fetch('ev_tranges')
-
-    Returns
-    -------
-    tranges_w_same_cond_bef : np.ndarray
-        Time ranges of those seconds that have the same condition in the second before.
-    """
-    tranges_flatten = tranges.flatten()
-    # difference between all tracked event times (also off times)
-    tranges_diff_all = np.diff(tranges_flatten)
-    # delete every second element because that is the stimulation length of 1s
-    tranges_diff = np.delete(tranges_diff_all, np.arange(0, tranges_diff_all.size, 2))
-    # get the idx where a pause is happening
-    idx_pause = np.where(tranges_diff < 0.1)
-    # add +1 to get the correct index
-    idx_onset = np.add(idx_pause, 1)
-    # get all real opto ON times with no opto stim in the second before
-    onset_time_w_same_cond_bef = tranges[idx_onset, 0]
-
-    evtranges = np.empty((1, 2))
-    # creating an event time ranges vector that has the true ON and OFF times
-    for i in range(idx_onset.shape[0]):
-        evon = onset_time_w_same_cond_bef[i]
-        evoff = onset_time_w_same_cond_bef[i] + 1
-
-        evtranges_i = np.column_stack((evon, evoff))
-        evtranges = np.row_stack((evtranges, evtranges_i))
-    tranges_w_same_cond_bef = evtranges[1:, :]
-    return tranges_w_same_cond_bef
-
-
-def psth_cond_trace(key, offsets=[-0.5, 0.5], figsize=None, title=True, ax=None):
-    """Plot PSTH of the 4 conditions resulting of the hollymov stimulus with opto/no opto
-    continuous condition as a baseline. The baseline of the continuous conditions is calculated
-     as means of the spike firing rate.
-
-    Parameters
-    ----------
-    key : dict
-        Key of the form {'m': ..., 's': ..., 'e': ...}. Could also specify 'u' the unit ID.
-        If the unit is not specify, all units are plotted.
-    offsets: np.ndarray, optional
-        Defines the time range of the PSTH w.r.t. the potential opto switch at time 0.
-    figsize : int
-         optionally define figure size
-    title : bool
-        set True to get title that specifies the mouse, series, experiment, and unit
-    ax :
-        initial input
-
-    Returns
-    -------
-    axes : list of objects
-        figure with one subplot per unit
-     """
-    # adjust offset wrt to get_psth()
-    offset = [offsets[0], -(1 - offsets[1])]
-
-    # get stimulus time ranges
-    stim_tranges, _ = (Stimulus.Trial() & key).get_tranges()
-
-    evkey = key
-    evkey.update({'ev_chan': 'opto1'})
-
-    # fetch the event times & get array with all time ranges
-    evtimesopto = (Event.Times() & evkey).fetch('ev_tranges')
-    if evtimesopto.size == 0:
-        print('No opto events populated in Event.Times() for unit '
-              '{:s} s{:02d} e{:02d} u{:02d}.\n  Skip plotting.'.format(
-                                            evkey['m'], evkey['s'], evkey['e'], evkey['u']))
-    else:
-        tranges_all = get_all_tranges(evkey)
-
-        # get the opto, no opto mask
-        _, _, opto = (Unit.Spikes() & key).get_tranges(tranges=tranges_all)
-        no_opto = ~opto
-
-        # get array with time ranges depending on conditon
-        tranges_opto = tranges_all[opto]
-        tranges_noopto = tranges_all[no_opto]
-
-        # list with all conditions:
-        # (1) opto on switch / suppression off switch
-        # (2) opto on continuity / suppression continuously off
-        # (3) opto off switch / suppression on switch
-        # (4) no opto continuity / suppression continuously on
-        tranges_conds = [get_tranges_cond_swit(tranges_opto),
-                         get_tranges_cond_cont(tranges_opto),
-                         get_tranges_cond_swit(tranges_noopto),
-                         get_tranges_cond_cont(tranges_noopto),
-                        ]
-        labels = ['on-off', 'off-off', 'off-on','on-on']
-        colors = ['tab:blue', 'tab:blue', 'k', 'k']
-        lines = ['-', ':', '-', ':']
-        widths = [1.5, 3, 1.5, 3]
-        visible = [1.0, 0.2, 1.0, 0.2]
-
-        # loop through conditions and get psths
-        psths_cond = []
-        midbins_cond = []
-        for cond in range(len(tranges_conds)):
-            # only tranges that are in stimulus time ranges
-            tranges_cond = tranges_conds[cond]
-            tranges_cond_stim = tranges_cond[
-                util.intersect_tranges(stim_tranges, tranges_cond)]
-
-            midbins, psths, _, _, _ = (Unit.Spikes() & key).get_psths(offsets=offset,
-                                                                   tranges=tranges_cond_stim)
-            # list (len=num_cond) of dicts with key as uid
-            psths_cond.append(psths)
-            midbins_cond.append(midbins)
-
-        # plotting
-        # save initial ax input. This is important if several units are plotted in the loop
-        # because ax will be overwritten
-        ax_init = ax
-        uids = sorted(psths_cond[0])
-        keys = (Unit.Spikes() & key).fetch(dj.key)
-        axes = []
-        for uid, key in zip(uids, keys):
-            assert uid == key['u']  # sanity check
-            if ax_init is None:
-                f, a = plt.subplots(figsize=figsize)  # make a new figure and axes
-            else:
-                a = ax
-            for cond in range(len(tranges_conds)):
-                a.plot(midbins_cond[cond],
-                       psths_cond[cond][uid],
-                       ls='-', marker='None',
-                       label=labels[cond],
-                       c=colors[cond],
-                       lw=widths[cond],
-                       alpha=visible[cond],
-                       zorder=2)
-            a.axvline(x=0, linestyle=':', c='grey', zorder=1)
-            a.set_xlabel("Time (s)")
-            a.set_ylabel("Firing rate (Hz)")
-            a.legend(title=' L6 CT feedback', frameon=False, fontsize=13)
-
-            titlestr = '%s s%02d e%02d u%02d PSTH' % (key['m'], int(key['s']), int(key['e']),
-                                                      uid)
-            if title:
-                a.set_title(titlestr)
-
-            axes.append(a)
-
-        return axes
-
-
 def psth_cond_base(key, offsets=[-0.5, 0.5], figsize=None, title=True, ax=None):
     """Plot PSTH of the 4 conditions resulting of the hollymov stimulus with opto/no opto
     continuous condition as baseline. The baseline of the continuous conditions is calculated
@@ -375,7 +98,7 @@ def psth_cond_base(key, offsets=[-0.5, 0.5], figsize=None, title=True, ax=None):
     evkey.update({'ev_chan': 'opto1'})
 
     # get all time ranges
-    tranges_all = get_all_tranges(evkey)
+    tranges_all = hmov.get_all_tranges(evkey)
 
     # get the opto, no opto mask
     _, _, opto = (Unit.Spikes() & key).get_tranges(tranges=tranges_all)
@@ -389,15 +112,15 @@ def psth_cond_base(key, offsets=[-0.5, 0.5], figsize=None, title=True, ax=None):
     # (1) opto on switch
     # (2) opto off switch
     tranges_swits = [
-        get_tranges_cond_swit(tranges_opto),
-        get_tranges_cond_swit(tranges_noopto),
+        hmov.get_tranges_cond_swit(tranges_opto),
+        hmov.get_tranges_cond_swit(tranges_noopto),
                     ]
     # list with continuous conditions:
     # (3) opto on continuity
     # (4) no opto continuity
     tranges_conts = [
-        get_tranges_cond_cont(tranges_opto),
-        get_tranges_cond_cont(tranges_noopto),
+        hmov.get_tranges_cond_cont(tranges_opto),
+        hmov.get_tranges_cond_cont(tranges_noopto),
                     ]
     labels_swit = ['on-off', 'off-on']
     labels_cont = ['off-off', 'on-on']
@@ -496,7 +219,7 @@ def bar_cond(m, s, e, u):
     evkey.update({'ev_chan': 'opto1'})
 
     # get all time ranges
-    tranges_all = get_all_tranges(evkey)
+    tranges_all = hmov.get_all_tranges(evkey)
 
     # get the opto, no opto mask
     _, _, opto = (Unit.Spikes() & key).get_tranges(tranges=tranges_all)
@@ -511,10 +234,10 @@ def bar_cond(m, s, e, u):
     # (2) opto on continuity
     # (3) opto off switch
     # (4) no opto continuity
-    tranges_conds = [get_tranges_cond_swit(tranges_opto),
-                     get_tranges_cond_cont(tranges_opto),
-                     get_tranges_cond_swit(tranges_noopto),
-                     get_tranges_cond_cont(tranges_noopto)]
+    tranges_conds = [hmov.get_tranges_cond_swit(tranges_opto),
+                     hmov.get_tranges_cond_cont(tranges_opto),
+                     hmov.get_tranges_cond_swit(tranges_noopto),
+                     hmov.get_tranges_cond_cont(tranges_noopto)]
     fr_prior = []
     fr_after = []
     std_prior = []
