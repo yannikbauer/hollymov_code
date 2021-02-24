dc8cef598bdc615a81c51665351a345372abdb54
yannikbauer <y.bauer@bio.lmu.de>
2021-02-24 15:24:56 +0100

    upsate setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name = 'hmov_code',
     url = 'https://gitlab.lrz.de/blab/hollymov_code',
-    description = ('Additional tools for analysing the Hollywood movie experiments.'),
+    description = ('Analysis code for hollywood movie experiments incl. dLGN recordings and '
+                   'V1 L6 (primary visual cortex Layer 6) cortico-thalamic feedback direct '
+                   'suppression (DFG grant "CRC1233 - Robust Vision", TP13).'),
     long_description = open('README.md').read(),
-    author = 'Lisa Schmors',
+    author = 'Lisa Schmors @ Berens Lab, U Tuebingen; Yannik Bauer @ Busse Lab, LMU Munich',
     author_email='lisa.schmors@uni-tuebingen.de',
-    version = '0.0.0',
+    version = '0.0.1',
     packages=find_packages(exclude=['tests*']),
     )
